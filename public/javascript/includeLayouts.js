5d1bbfe9ee926ca216a720e28ce18cb9b02eb6c2
GitJaeSung <gkswotjd567@gmail.com>
2021-03-10 15:33:20 +0900

    update

---
+++ public/javascript/includeLayouts.js
@@ -0,0 +1,37 @@
+function includeHTML(callback) {
+
+    var elment_Size, lenght, elment, file, xhr;
+    elment_Size = document.getElementsByTagName("*");
+
+    for(length = 0; length <elment_Size.length; length++) {
+
+        elment = elment_Size[length];
+        file = elment.getAttribute("include_layouts");
+
+        if(file) {
+
+            xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function() {
+
+                if(this.readyState == 4) {
+
+                    if(this.status == 200) { elment.innerHTML = this.responseText; }
+                    if(this.status == 404) { elment.innerHTML = "Page not found."; }
+
+                    elment.removeAttribute("include_layouts");
+                    includeHTML(callback);
+                    
+                }
+
+            };
+
+            xhr.open("GET", file, true);
+            xhr.send();
+            return;
+
+        }
+    }
+
+    setTimeout(function() { callback(); }, 0);
+}
