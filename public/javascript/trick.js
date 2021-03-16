0fa6551faffa6f6b309088b9545a44a6f21efbb4
GitJaeSung <gkswotjd567@gmail.com>
2021-03-16 20:27:48 +0900

    update

--- public/javascript/trick.js
+++ public/javascript/trick.js
@@ -1,5 +1,5 @@
 //Trick to change html
 
-function home() {document.getElementById("post").innerHTML='<object type="text/html" data="/layout/home.html" style="height:100%;width:100%"></object>';}
-function about() {document.getElementById("post").innerHTML='<object type="text/html" data="/layout/about.html" style="height:100%;width:100%"></object>';}
-function post() {document.getElementById("post").innerHTML='<object type="text/html" data="/layout/post.html" style="height:100%;width:100%"></object>';}
+function home() { document.getElementById("post").innerHTML = '<object type="text/html" data="/layout/home.html" style="height:100%; width:100%"></object>'; }
+function about() { document.getElementById("post").innerHTML = '<object type="text/html" data="/layout/about.html" style="height:100%; width:100%"></object>'; }
+function post() { document.getElementById("post").innerHTML = '<object type="text/html" data="/layout/post.html" style="height:100%; width:100%"></object>'; }
